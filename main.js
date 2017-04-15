13d3d10fe398ae27936faa03541ea163f2661266
Luke Miles <rashreportloaf@gmail.com>
2017-04-15 19:43:22 -0400

    put all the scripting in the js file

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-current_node_count = 0;
+var s = new sigma('innergraphbox');
+
+var totalNumNodes = 0;
+
+function addRandomNode() {
+  totalNumNodes++;
+  s.graph.addNode({
+    id: 'n' + totalNumNodes.toString(),
+    label: 'node number ' + totalNumNodes.toString(),
+    x: Math.random(),
+    y: Math.random(),
+    size: 1,
+    color: '#F00'
+  });
+  s.refresh();
+}
 
 function addNode()
 {
     name = document.getElementById('nodeName').value;
     xPosition = parseFloat(document.getElementById('nodeX').value);
     yPosition = parseFloat(document.getElementById('nodeY').value);
-    current_node_count = current_node_count + 1;
+    totalNumNodes = totalNumNodes + 1;
   s.graph.addNode({
-    id: 'n' + current_node_count.toString(),
+    id: 'n' + totalNumNodes.toString(),
     label: name,
     x: xPosition,
     y: yPosition,
