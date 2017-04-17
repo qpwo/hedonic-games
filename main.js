ca6b5898cc14a436b6bd12f3e87bcbb6dbfac924
Luke Miles <rashreportloaf@gmail.com>
2017-04-16 22:29:14 -0400

    added partitions and coloring

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var s = new sigma("innergraphbox");
+var partition = [];
 
 function addNode(name, x, y) {
   // add a random node to the graph
@@ -11,7 +12,7 @@ function addNode(name, x, y) {
     x: (x ? x : Math.random()),
     y: (y ? y : Math.random()),
     size: 1,
-    color: '#F00'
+    color: '#000'
   });
   s.refresh();
 }
@@ -46,3 +47,26 @@ function drawGraphFromText() {
   s.refresh();
 }
 
+
+function makePartitionFromText() {
+  // set the partitions to the ones described by the user and color them
+  var bigString = document.getElementById('partitionTextField').value;
+  var lines = bigString.split('\n');
+  partition = lines.map(function(line) {return line.replace(/ /g, '').split(',');});
+  for (let coalition of partition) {
+    colorSubgraph(coalition, randomColor());
+  }
+}
+
+function colorSubgraph(nodes, color) {
+  // color a subgraph induced by a set of nodes
+  for (let nodeObject of s.graph.nodes(nodes)) {
+    nodeObject.color = color;
+  }
+  s.refresh();
+}
+
+function randomColor() {
+  // generates a random hex color code
+  return '#'+Math.random().toString(16).substr(-6);
+}
